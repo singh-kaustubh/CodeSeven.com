45547ad357ca109dd9f31828daf65c98ab647991
singh-kaustubh <kaustubhsingh38192@gmail.com>
2022-06-29 22:36:40 +0530

    main

--- pages/api/updateProduct.js
+++ pages/api/updateProduct.js
@@ -1,10 +1,10 @@
 import Product from "../../models/Product";
-import connectDb from "../../middleware/mongoose";
+import connectDb from "../../middlewear/mongoose";
 
 const handler = async (req, res) => {
-  if (req.method == "POST") {
+  if (req.method == "PUT") {
     for (let i = 0; i < req.body.length; i++) {
-      let p = await Product.findByIdAndUpdate(req.body[i]._id, req.body[i]);
+      await Product.findByIdAndUpdate(req.body[i]._id, req.body[i]);
     }
     res.status(200).json({ success: "success" });
   } else {
