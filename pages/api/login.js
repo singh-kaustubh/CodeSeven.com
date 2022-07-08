d8922a9cbf4cdd8efd0f84d76fa74347d08a113f
singh-kaustubh <kaustubhsingh38192@gmail.com>
2022-07-08 21:59:04 +0530

    main

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,15 @@
 import User from "../../models/User";
 import connectDb from "../../middlewear/mongoose";
+import CryptoJS from "crypto-js";
 const handler = async (req, res) => {
+  const secKey = process.env.SECRET_KEY_CRYPTO.toString();
   if (req.method === `POST`) {
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
-        if (
-          req.body.email == user.email &&
-          req.body.password == user.password
-        ) {
+        const bytes = CryptoJS.AES.decrypt(user.password, secKey);
+        const decryptPass = bytes.toString(CryptoJS.enc.Utf8);
+        if (req.body.email == user.email && req.body.password == decryptPass) {
           return res
             .status(200)
             .json({ success: true, name: user.name, email: user.email });
