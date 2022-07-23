e20178644a6f7eae091079230ec66a0997b731f8
Kaustubh Kumar Singh <93437624+singh-kaustubh@users.noreply.github.com>
2022-07-23 12:11:18 +0530

    admin dashboard

---
+++ src/layouts/header/ProfileDD.js
@@ -0,0 +1,76 @@
+import React from "react";
+import FeatherIcon from "feather-icons-react";
+import Image from "next/image";
+import userimg from "../../../assets/images/users/user2.jpg";
+import {
+  Box,
+  Menu,
+  Typography,
+  Link,
+  ListItemButton,
+  List,
+  ListItemText,
+  Button,
+  Divider,
+} from "@mui/material";
+const ProfileDD = () => {
+  const [anchorEl4, setAnchorEl4] = React.useState(null);
+
+  const handleClick4 = (event) => {
+    setAnchorEl4(event.currentTarget);
+  };
+
+  const handleClose4 = () => {
+    setAnchorEl4(null);
+  };
+  return (
+    <>
+      <Button
+        aria-label="menu"
+        color="inherit"
+        aria-controls="profile-menu"
+        aria-haspopup="true"
+        onClick={handleClick4}
+      >
+        <Box display="flex" alignItems="center">
+          <Image
+            src={userimg}
+            alt={userimg}
+            width="30"
+            height="30"
+            className="roundedCircle"
+          />
+          <Box
+            sx={{
+              display: {
+                xs: "none",
+                sm: "flex",
+              },
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              color="textSecondary"
+              variant="h5"
+              fontWeight="400"
+              sx={{ ml: 1 }}
+            >
+              Hi,
+            </Typography>
+            <Typography
+              variant="h5"
+              fontWeight="700"
+              sx={{
+                ml: 1,
+              }}
+            >
+              Julia
+            </Typography>
+          </Box>
+        </Box>
+      </Button>
+    </>
+  );
+};
+
+export default ProfileDD;
---
+++ src/layouts/sidebar/MenuItems.js
@@ -0,0 +1,34 @@
+const Menuitems = [
+  {
+    title: "Dashbaord",
+    icon: "home",
+    href: "/admin",
+  },
+  {
+    title: "Add product",
+    icon: "disc",
+    href: "/admin/addProduct",
+  },
+  {
+    title: "View Product",
+    icon: "layout",
+    href: "/admin/viewProduct",
+  },
+  {
+    title: "Remove Product",
+    icon: "info",
+    href: "/admin/removeProduct",
+  },
+  {
+    title: "Users",
+    icon: "star",
+    href: "/admin/Users",
+  },
+  {
+    title: "Orders",
+    icon: "image",
+    href: "/admin/orders",
+  },
+];
+
+export default Menuitems;
